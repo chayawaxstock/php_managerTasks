8d2ae3dfafb7bc328558b646fe783d6bbb545170
chayawaxstock <c0556777462@gmail.com>
2018-12-09 16:26:02 +0200

    ww

---
+++ managerTasks-chaya-new/API/controllers/presenceday_controller.php
@@ -0,0 +1,15 @@
+<?php
+
+class presenceday_controller
+{
+     var $presenceday_service;
+
+    function __construct() {
+        $this->presenceday_service = new presenceday_service();
+    }
+    
+      function update_presenceday_worker($presence_day){
+	return $this->presenceday_service->update_presenceday_worker($presence_day);
+    }
+}
+
